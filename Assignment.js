161af8ee062e7504f535799476cbc79e0574e5ac
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2020-03-25 13:53:00 -0700

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,9 @@
+var name = "Chiamaka";
+var arr = ["Html", "Css", "Javascript","Design"];
+
+console.log("arr");
+
+var arr;
+for (let index = 0; index <= 200; index = index + 2) {
+    console.log(index);
+}
